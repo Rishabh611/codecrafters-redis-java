b32bf1859033dcb79f4e937ec4dd7bf59c050429
Rishabh <rishabsinghrock@gmail.com>
2024-11-06 10:36:45 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 	    // You can use print statements as follows for debugging, they'll be visible when running tests.
 	    System.out.println("Logs from your program will appear here!");
 
 		ServerSocket serverSocket = null;
-		Socket clientSocket = null;
 		int port = 6379;
 
 		try {
@@ -17,8 +16,19 @@ public class Main {
 			// ensures that we don't run into 'Address already in use' errors
 			serverSocket.setReuseAddress(true);
 			// Wait for connection from client.
-			clientSocket = serverSocket.accept();
+			while(true) {
+				Socket clientSocket = serverSocket.accept();
+				Thread clientThread = new Thread(() -> handleClient(clientSocket));
+				clientThread.start();
+			}
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+		}
 
+
+  	}
+	private static void handleClient(Socket clientSocket) {
+		try {
 			BufferedReader in = new BufferedReader(
 					new InputStreamReader(clientSocket.getInputStream())
 			);
@@ -37,15 +47,14 @@ public class Main {
 				}
 			}
 		} catch (IOException e) {
-			System.out.println("IOException: " + e.getMessage());
+			System.out.println("IOException " + e.getMessage());
 		} finally {
 			try {
-				if (clientSocket != null) {
-					clientSocket.close();
-				}
+				clientSocket.close();
 			} catch (IOException e) {
-				System.out.println("IOException: " + e.getMessage());
+				System.out.println("Error closing client socket: " + e.getMessage());
 			}
 		}
-  	}
+
+	}
 }
